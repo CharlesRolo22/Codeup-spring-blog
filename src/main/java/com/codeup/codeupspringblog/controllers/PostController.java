842df62f72b40d93f5b0363aefd7e6ddb47ba16b
charlesrolo22 <charles.a.roland@gmail.com>
2023-06-08 14:57:03 -0500

    new update

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,44 +1,83 @@
+
+
 package com.codeup.codeupspringblog.controllers;
 
 import com.codeup.codeupspringblog.models.Post;
+import com.codeup.codeupspringblog.models.PostCategories;
+import com.codeup.codeupspringblog.models.User;
+import com.codeup.codeupspringblog.repositories.PostCategoriesRepository;
+import com.codeup.codeupspringblog.repositories.PostRepository;
+import com.codeup.codeupspringblog.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class PostController {
+    private final PostRepository postsDao;
+    private final UserRepository userDao;
+    private final PostCategoriesRepository catDao;
+
+    public PostController(PostRepository postsDao, UserRepository userDao, PostCategoriesRepository catDao) {
+        this.postsDao = postsDao;
+        this.userDao = userDao;
+        this.catDao = catDao;
+    }
 
     @GetMapping("/posts")
+
     public String viewPosts(Model model) {
-        List<Post> posts = new ArrayList<>();
-        posts.add(new Post("First post", "This is my first post"));
-        posts.add(new Post("Second post", "This is my second post"));
-        model.addAttribute("posts", posts);
+        model.addAttribute("posts", postsDao.findAll());
         return "posts/index";
     }
 
+
     @GetMapping("/posts/{id}")
     public String singlePost(@PathVariable long id, Model model) {
-        Post post = new Post("Title", "Description");
-        model.addAttribute("post", post);
-
+        model.addAttribute("post", postsDao.findById(id).get());
         return "posts/show";
     }
 
-    @GetMapping("/posts/create")
-    public String showPostForm() {
 
+    @GetMapping("/posts/create")
+    public String showPostForm(Model model) {
+        // show categories in form
+        model.addAttribute("categories", catDao.findAll());
+        // pass a new Post object to the form
+        model.addAttribute("post", new Post());
+        return "/posts/create";
+    }
+
+
+    @PostMapping("/posts/create")
+//    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    public String submitNewPost(@ModelAttribute Post post) {
+        User user = userDao.findById(1L).get();
+        post.setUser(user);
+        postsDao.save(post);
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/posts/{id}/edit")
+    public String showEditForm(@PathVariable long id, Model model) {
+        if(postsDao.findById(id).isPresent()) {
+            Post postToEdit = postsDao.findById(id).get();
+            model.addAttribute("post", postToEdit);
+        }
         return "posts/create";
     }
 
-    @PostMapping("/posts/create")
-    public String submitNewPost() {
-
+    @PostMapping("/posts/{id}/edit")
+    public String updatePost(@ModelAttribute Post newPost) {
+        User user = userDao.findById(1L).get();
+        newPost.setUser(user);
+        postsDao.save(newPost);
         return "redirect:/posts";
     }
+
+
+
 }
---
+++ src/main/java/com/codeup/codeupspringblog/models/User.java
@@ -0,0 +1,32 @@
+package com.codeup.codeupspringblog.models;
+
+
+import com.codeup.codeupspringblog.models.Post;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, columnDefinition = "VARCHAR(100)")
+    private String username;
+    @Column(nullable = false, columnDefinition = "VARCHAR(100)")
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    List<Post> posts;
+}
